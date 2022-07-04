6ff8a6adbd656b56fdc6f91bca8b0336d229e408
Oscar Jose Garcia Rosales <oscar.garcia1235641@gmail.com>
2022-07-04 16:14:24 -0600

    otro más

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>oooooohsss
+      <Text>ok
       
       </Text>
       <StatusBar style="auto" />
