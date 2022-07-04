1678c3dd58705edcdb142af08a1fc516d5fda162
Oscar Jose Garcia Rosales <oscar.garcia1235641@gmail.com>
2022-07-04 15:45:14 -0600

    cuarto commit

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>alignIt
+      <Text>muucho cambios
+        
       
       </Text>
       <StatusBar style="auto" />
