1066b019454c6d9c0d7c6ce46d383c9b0a1a9473
Oscar Jose Garcia Rosales <oscar.garcia1235641@gmail.com>
2022-07-05 13:19:50 -0600

    commit con estilos

--- App.js
+++ App.js
@@ -1,23 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {StyleSheet,TouchableOpacity, Text, View,Image} from 'react-native';
+import laImg from './assets/img3.png';
 
 export default function App() {
   return (
+    
     <View style={styles.container}>
-      <Text>definitivo
+     
+      <Text style={styles.texto}>
+        Bienvenido a la Dogería!
+      </Text> 
+      <Image
+      source={laImg}
+      style={styles.img}
+      ></Image>
+      <TouchableOpacity
+       style={styles.boton}>
+        <Text 
+        style={styles.botonText}>
+          Ordena Aqui!
+        </Text>
+      </TouchableOpacity>
 
-      
-      </Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flex: 2,
+    backgroundColor: 'yellow',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    },
+  
+    texto:{
+      color:'red',
+      fontSize:30,
+    margin:50},
+
+    img:{margin:100,
+      borderRadius:150,
+        height: 300,
+        width: 300},
+
+    boton:{
+      flex:1,
+          height:100,
+          width:80
+        },
+        botonText:{
+          backgroundColor:'red'
+        },
+  
+   
+
+  
 });
